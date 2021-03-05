3af685902e50984b2de7071990a6688e723634a8
Astoremica <misawashi4zy@gmail.com>
2021-03-05 15:05:28 +0900

    add main-storyboard

--- TryRealm/ViewController.swift
+++ TryRealm/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldAge: UITextField!
+    @IBOutlet weak var saveRealmButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textFieldName.placeholder = "氏名を入力してください"
+        textFieldAge.placeholder = "年齢を入力してください"
+    
+        saveRealmButton.layer.cornerRadius = 22
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
 }
 
